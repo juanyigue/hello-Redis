e8ffc048ec8f5f4760fdff5a7c35f17d601b0437
Juan Pablo <Juan Pablo>
2016-04-14 10:56:34 -0300

    first commit

---
+++ hello-redis/hello-redis/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RedisAccess;
+
+namespace hello_redis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RedisDB rdb = new RedisDB();
+            string key = "t-1";
+            string value = "test 1 value";
+
+            rdb.SetValue(key, value);
+
+            Console.WriteLine(string.Format("Value of {0}: {1}", key, value));
+        }
+    }
+}
