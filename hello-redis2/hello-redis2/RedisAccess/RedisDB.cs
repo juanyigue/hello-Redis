81d96b73082e727fd28b3fe1c415d3b7494441cd
Juan Pablo <Juan Pablo>
2016-04-15 12:04:14 -0300

    redis2

---
+++ hello-redis2/hello-redis2/RedisAccess/RedisDB.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisAccess
+{
+    public class RedisDB
+    {
+        public RedisDB()
+        {
+
+        }
+
+        public void SetValue(string key, string value)
+        {
+
+        }
+
+        public string GetValue(string key)
+        {
+            return "lala";
+        }
+
+    }
+}
