c5238530030567248f46e9c1edd829d56b44d112
Juan Pablo <Juan Pablo>
2016-04-18 08:45:42 -0300

    Endpoint fixed 2

---
+++ hello-redis3/RedisAccess/RedisDB.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using CSRedis;
+
+namespace RedisAccess
+{
+    public class RedisDB
+    {
+
+        private RedisClient redisCli;
+
+        public RedisDB()
+        {
+        }
+
+        public RedisDB(string host, string port)
+        {
+            redisCli = new RedisClient(host, Convert.ToInt32(port));
+        }
+
+        public string Ping()
+        {
+            return redisCli.Ping();
+        }
+
+        public string SayHello()
+        {
+            return redisCli.Echo("hello world from Redis");
+        }
+
+        public DateTime GiveTime()
+        {
+            return redisCli.Time();
+        }
+
+        //public void test2(string host, string port)
+        //{
+        //    using (var redis = new RedisClient(host, Convert.ToInt32(port)))
+        //    {
+        //        string ping = redis.Ping();
+        //        string echo = redis.Echo("hello world from Redis");
+        //        DateTime time = redis.Time();
+        //    }
+        //}
+
+        public void SetValue(string key, string value)
+        {
+
+        }
+
+        public string GetValue(string key)
+        {
+            return "lala";
+        }
+
+    }
+}
---
+++ hello-redis3/hello-redis3/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RedisAccess;
+
+namespace hello_redis3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //test1();
+            test2();
+        }
+
+        private static void test1()
+        {
+            RedisDB rdb = new RedisDB();
+            string key = "t-1";
+            string value = "test 1 value";
+
+            rdb.SetValue(key, value);
+
+            Console.WriteLine(string.Format("Value of {0}: {1}", key, value));
+        }
+
+        private static void test2()
+        {
+            try
+            {
+                Console.WriteLine("Write the host to connect to Redis: ");
+                string host = Console.ReadLine();
+                Console.WriteLine("Now write the port that Redis is listening to: ");
+                string port = Console.ReadLine();
+
+                RedisDB rdb = new RedisDB(host, port);
+
+                Console.WriteLine("Doing ping to Redis...");
+                Console.WriteLine(string.Format("Answer from Redis: {0}", rdb.Ping()));
+                Console.WriteLine(string.Format("Saying hello from Redis: {0}", rdb.SayHello()));
+                Console.WriteLine(string.Format("Redis, give me time: {0}", rdb.GiveTime().ToShortTimeString()));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(string.Format("An error has ocurred: {0}", ex));
+            }
+            finally
+            {
+                Console.WriteLine("\nPress any key to finish...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
